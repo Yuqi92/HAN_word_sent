6a186a6b166fb3ddd0d06782a3a611dc55fa3b5f
ys2887@columbia.edu <ys2887@columbia.edu>
2019-08-06 11:57:44 -0500

    update

---
+++ extract_patient_vector.py
@@ -0,0 +1,110 @@
+# extract patient vector of obesity from two-level HAN
+import parameters
+import tensorflow as tf
+import logging
+from Embedding import Embedding
+import numpy as np
+import pandas as pd
+import math
+from tqdm import tqdm
+from utility import HAN_model
+
+logging.basicConfig(filename=parameters.log_file_name, level=logging.INFO)
+
+train_csv = pd.read_csv(parameters.obesity_train_label)
+
+train_patient_name = list(train_csv['patient_id'])
+n_patient = len(train_patient_name)
+def generate_token_embedding(subject_id):
+    x_doc = np.ones([parameters.max_document_length,
+                      parameters.max_sentence_length], dtype=np.int32) * Embedding().get_pad_index()
+    patient_file = parameters.obesity_train_file_folder + str(subject_id) + '.txt'
+
+    word_length_per_doc = []
+    text = open(patient_file)
+    num_sent_per_doc = 0
+    for current_sent_index, line in enumerate(text):
+        num_sent_per_doc += 1
+        if current_sent_index < parameters.max_document_length:
+            strip_line = line.strip()
+            word_list = strip_line.split()
+            num_word_per_sent = len(word_list)
+            if num_word_per_sent <= parameters.max_sentence_length:
+                word_length_per_doc.append(num_word_per_sent)
+            else:
+                word_length_per_doc.append(parameters.max_sentence_length)
+            for current_word_index, word in enumerate(word_list):
+                    if current_word_index < parameters.max_sentence_length:
+                        x_doc[current_sent_index][current_word_index] = Embedding().get_word_index(word=word.lower(),
+                                                                                                   unknown_word="UNK")
+        else:
+            num_sent_per_doc = parameters.max_document_length
+            continue
+    num_word_per_doc = word_length_per_doc + [0] * (parameters.max_document_length - num_sent_per_doc)
+    text.close()
+    return x_doc, num_sent_per_doc, num_word_per_doc
+
+def load_x_data_for_HAN(patient_name, keep_prob, input_x, sentence_lengths, word_lengths, dropout_keep_prob,
+                        is_training_ph, is_training_value):
+
+    generate_token_embedding_results = []
+    for i in patient_name:
+        generate_token_embedding_results.append(generate_token_embedding(i))
+
+    tmp_x = np.zeros([len(generate_token_embedding_results),
+                            parameters.max_document_length,
+                            parameters.max_sentence_length], dtype=np.float32)
+    sent_l = []
+    word_l = []
+
+    for (M, r) in enumerate(generate_token_embedding_results):
+
+        tmp_x[M] = r[0]
+        sent_l.append(r[1])
+        word_l.append(r[2])
+
+    sent_l = np.asarray(sent_l)
+    word_l = np.stack(word_l)
+    feed_dict = {input_x: tmp_x,
+                 sentence_lengths: sent_l,
+                 word_lengths: word_l,
+                 dropout_keep_prob: keep_prob,
+                 is_training_ph: is_training_value}
+
+    return feed_dict
+
+#define placeholder
+
+input_ys = []
+for i in range(parameters.multi_size):
+    input_ys.append(tf.placeholder(tf.int32, [None,parameters.num_classes], name="input_y"+str(i)))
+
+input_x = tf.placeholder(tf.int32,
+                         [None, parameters.max_document_length, parameters.max_sentence_length],name="input_x")
+is_training = tf.placeholder(tf.bool, [], name="is_training")
+word_lengths = tf.placeholder(shape=(None, parameters.max_document_length), dtype=tf.int32, name='word_lengths')
+sentence_lengths = tf.placeholder(shape=(None,), dtype=tf.int32, name='sentence_lengths')
+dropout_keep_prob = tf.placeholder(tf.float32, [], name="dropout_keep_prob")
+
+_, _, patient_vector = HAN_model(input_x,input_ys,word_lengths,sentence_lengths, 
+	is_training, dropout_keep_prob,
+	Embedding().get_embedding_numpy())
+
+
+saver = tf.train.Saver()
+num_batch = int(math.ceil(n_patient / parameters.n_batch))
+
+with tf.Session() as sess:
+    saver.restore(sess, parameters.deep_learning_model)
+
+    for i in tqdm(range(num_batch)):
+        tmp_train_patient_name = train_patient_name[i*parameters.n_batch:min((i+1)*parameters.n_batch, n_train)]
+
+        feed_dict = load_x_data_for_HAN(tmp_train_patient_name, parameters.drop_out_train, input_x, 
+                    sentence_lengths, word_lengths, dropout_keep_prob,is_training, True)
+
+        tmp_patient_vector = sess.run(patient_vector, feed_dict=feed_dict)
+
+        for j in range(len(tmp_train_patient_name)):
+            np.save(parameters.obesity_vector_directory + tmp_train_patient_name[j] + ".npy", tmp_patient_vector[j])
+
--- parameters.py
+++ parameters.py
@@ -6,16 +6,16 @@ index_test_path = index_path + 'test.index.npy'
 index_train_path = index_path + 'train.index.npy'
 
 restore = False
-model_path = './results/3_task_dead/'
+model_path = './new_results/uni_task/one_year_640/'
 deep_learning_model = model_path + "model.weights/model.ckpt"
 
-max_document_length = 1000 # how many sentences in document(max): n_sent
+max_document_length = 640 # how many sentences in document(max): n_sent
 max_sentence_length = 25  # how many words in the sentence(max): n_word
 embedding_size = 100
 
 task = 'dead' # option:  dead; LOS; ICD9
 tasks_icd_9 = []
-tasks_dead_date = [0, 31, 366]
+tasks_dead_date = [366]
 tasks_los_date = []
 
 multi_size = len(tasks_dead_date)
@@ -29,7 +29,7 @@ log_file_name = model_path + 'log.log'
 
 
 
-n_batch = 32
+n_batch = 50
 early_stop_times = 5
 num_classes = 2
 model_type = "HAN"
